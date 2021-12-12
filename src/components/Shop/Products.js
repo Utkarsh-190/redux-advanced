84fd5fe2fd9589470d7eef06d50019c8e675b040
Utkarsh_190 <utkarshtyagi62@gmail.com>
2021-12-12 15:24:07 +0530

    created cart slice for adding and removing items in cart

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,5 +1,20 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 6,
+    title: "Book",
+    description: "First book I ever wrote",
+  },
+  {
+    id: "p2",
+    price: 5,
+    title: "Pen",
+    description: "First pen of my life",
+  },
+];
 
 const Products = (props) => {
   return (
@@ -7,9 +22,9 @@ const Products = (props) => {
       <h2>Buy your favorite products</h2>
       <ul>
         <ProductItem
-          title='Test'
+          title="Test"
           price={6}
-          description='This is a first product - amazing!'
+          description="This is a first product - amazing!"
         />
       </ul>
     </section>
